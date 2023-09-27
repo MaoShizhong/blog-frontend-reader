4aa07964d0c8e42d50da96ccd1252a7cc89a2f77
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-09-27 23:56:17 +0100

    Removed broken import

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { Header } from './components/header/Header';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useEffect, useState, createContext, Dispatch, SetStateAction } from 'react';
 import { fetchData } from './helpers/fetch_options';
-import { AvatarColour } from './components/profile/ColourPicker';
 
 export type User =
     | {
           id: string;
           username: string;
-          avatar: AvatarColour;
+          avatar: string;
           bookmarkedPosts: string[];
       }
     | undefined;
